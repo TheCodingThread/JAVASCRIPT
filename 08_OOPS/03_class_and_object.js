6c75293cf7b1144761979e91db3aaaed2783435d
TheStarterOne <sebinmatheweapen@gmail.com>
2025-08-09 15:01:31 +0530

    Classes and objects in JavaScript

---
+++ 08_OOPS/03_class_and_object.js
@@ -0,0 +1,13 @@
+class Accessory{ //name of class should start with capital letter
+    constructor(company, model){ //to use constructor function inside a class, we use 'constructor'
+        this.company = company;
+        this.model = model;
+    }
+
+    details(){ //function inside a class is called method
+        return `The accessory is ${this.model} from ${this.company}`;
+    }
+}
+
+let item = new Accessory("Asus", "Tuf A15");
+console.log(item.details()); // The accessory is Tuf A15 from Asus
