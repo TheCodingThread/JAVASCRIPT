2ce90bf23936a268c001c286f64bb07d71d30445
TheStarterOne <sebinmatheweapen@gmail.com>
2025-08-09 15:04:31 +0530

    Applying inheritance in JavaScript

---
+++ 08_OOPS/04_inheritance.js
@@ -0,0 +1,19 @@
+class Accessory{
+    constructor(company, model){
+        this.company = company;
+        this.model = model;
+    }
+
+    details(){
+        return `The accessory is ${this.model} from ${this.company}`;
+    }
+}
+
+class Laptop extends Accessory { // Laptop class inherits from Accessory using 'extends' keyword
+    lap_details() {
+        return `The laptop is ${this.company} ${this.model}`;
+    }
+}
+
+let laptopItem = new Laptop("Asus", "Tuf A15");
+console.log(laptopItem.lap_details()); // The laptop is Asus Tuf A15
