3c46a991b3bc8ed2a199cf010661df62bb886f32
TheStarterOne <sebinmatheweapen@gmail.com>
2025-09-07 20:24:12 +0530

    Adding a new element when the button is clicked

---
+++ 09_DOM/ex4.js
@@ -0,0 +1,5 @@
+document.getElementById("btn4").addEventListener("click", function(){
+    let item = document.createElement("li")
+    item.textContent = "Eggs"
+    document.getElementById("ul1").appendChild(item)
+})
