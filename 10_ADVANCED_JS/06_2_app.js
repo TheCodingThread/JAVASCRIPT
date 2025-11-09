5d565138a4b1d5aa985f6a880ba922632e204a65
TheCodingThread <sebinmatheweapen@gmail.com>
2025-11-10 02:40:22 +0530

    Changed the naming of the files

--- 10_ADVANCED_JS/06_2_app.js
+++ 10_ADVANCED_JS/06_2_app.js
@@ -1,8 +1,8 @@
 //default import
-import div from "./06_1_mathOpModule.js";
+import div from "./06_1_mathOpESModule.js";
 
 //named import
-import {add} from "./06_1_mathOpModule.js"
+import {add} from "./06_1_mathOpESModule.js"
 
 console.log(div(5, 2))
 console.log(add(5, 2))
