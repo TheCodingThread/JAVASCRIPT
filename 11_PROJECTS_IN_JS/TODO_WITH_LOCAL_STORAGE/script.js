0fc1a69a12e12dc9c773b262749184273ab31391
TheCodingThread <sebinmatheweapen@gmail.com>
2026-01-07 06:19:17 +0530

    reading from local storage using DOM event

--- 11_PROJECTS_IN_JS/TODO_WITH_LOCAL_STORAGE/script.js
+++ 11_PROJECTS_IN_JS/TODO_WITH_LOCAL_STORAGE/script.js
@@ -1,28 +1,36 @@
-const todoIp = document.getElementById("todo-input")
-const addTaskBtn = document.getElementById("add-task-btn")
-const todoList = document.getElementById("todo-list")
+document.addEventListener("DOMContentLoaded", () => {
+  const todoIp = document.getElementById("todo-input");
+  const addTaskBtn = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-//adding a task in array
+  //adding a task in array
 
-let tasks = []
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || []; //parse function of JSON retrieves back the original form of the input which was converted to string
 
-addTaskBtn.addEventListener("click", () =>{
-    const taskText = todoIp.value.trim()
-    if(taskText === "") return;
+  tasks.forEach((task) => renderTask(task))
+
+  addTaskBtn.addEventListener("click", () => {
+    const taskText = todoIp.value.trim();
+    if (taskText === "") return;
 
     const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false
+      id: Date.now(),
+      text: taskText,
+      completed: false,
     };
 
-    tasks.push(newTask)
-    saveTasks() //write tasks to local storage
-    todoIp.value = ""
-    console.log(tasks)
-})
+    tasks.push(newTask);
+    saveTasks(); //write tasks to local storage
+    todoIp.value = "";
+    console.log(tasks);
+  });
 
-//writing tasks to local storage
-function saveTasks(){
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-}
+  function renderTask(task){
+    console.log(task);
+  }
+
+  //writing tasks to local storage
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks)); //setItem takes up key:value pair where both are strings
+  }
+});
